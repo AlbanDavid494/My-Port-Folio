bfee0db5621cb243ec53861d0a66a0c72b8293c7
AlbanDavid <albandavid494@gmail.com>
2023-03-26 08:04:16 -0700

    another changes

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
-const menu = document.querySelector(".header__links");
-const menuItems = document.querySelectorAll(".header__link");
-const hamburger = document.querySelector(".hamburger");
-const closeIcon = document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".header__link");
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
 
-function toggleMenu() {
-    if (menu.classList.contains("showMenu")) {
-        menu.classList.remove("showMenu");
-        closeIcon.style.display = "none";
-        menuIcon.style.display = "block";
-    } else {
-        menu.classList.add("showMenu");
-        closeIcon.style.display = "block";
-        menuIcon.style.display = "none";
-    }
-}
-
-hamburger.addEventListener("click", toggleMenu);
-
-
-
-header__link.forEach(
-    function(header__link) {
-        header__link.addEventListener("click", toggleMenu);
-    }
-)
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
